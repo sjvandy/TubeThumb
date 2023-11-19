89bb9b387cc88988b6cd8fc950fdbc926687223c
Steven Vandegrift <svandegrift@icloud.com>
2023-11-18 23:56:13 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# TubeThumb
+Youtube Thumbnail Downloader
+
+### Requirements
+- [Python3](https://www.python.org/downloads/)
+  
+> <i> Mac users need to go to Applications/Python3.x/ and select Install Certificates.command </i>
+
+I got tired of using those websites with tons of ads and misleading download links, so I made a script myself. Enjoy!
+
+## How To
+1. Navigate to the TubeTunes folder in terminal
+    - Copy Pathfile
+    - Open Terminal
+    - Type `cd ~pathfile_name`
+2. Type `pip3 install -r requirements.txt` into the terminal
+3. Type `Python3 TubeThumb.py`
+4. Enter Link to Youtube video
+5. Thumbnail will download to the user downloads folder
+
+
---
+++ TubeThumb.py
@@ -0,0 +1,40 @@
+#Youtube Thumbnail Downloader
+
+import os
+from pytube import exceptions
+from pytube import YouTube
+from urllib.request import urlretrieve
+
+isRunning = True
+
+downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+# Get Youtube Thumbnail
+def Get_Thumbnail(link):
+    yt = YouTube(link)
+    print("Downloading...")
+    thumbnail_url = yt.thumbnail_url
+    filename_path = f"{downloads_folder}/{yt.title}.png"
+    try:
+        urlretrieve(thumbnail_url, filename_path)
+    except Exception as e:
+        print(f"Error: {e}")
+
+#Program Welcome Prompt
+print("Youtube Thumbnail Downloader")
+print('To close program, hit option C or type "quit"')
+
+while isRunning:
+    user_input = input('Enter Youtube Link: ')
+    if user_input == 'quit':
+        isRunning = False
+        break
+    else:
+        try:
+            Get_Thumbnail(user_input)
+            print("Thumbnail Downloaded Successfully")
+        except Exception as e:
+            if isinstance(e, exceptions.RegexMatchError):
+                print('Error: Link is not a valid Youtube link. Try Again.')
+            else:
+                print(f"An Error Occured: {type(e)}")            
